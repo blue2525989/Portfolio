5279c7aa5de35100176adaf246d3f060efd41697
jason <jbierbrauer99@gmail.com>
2017-07-23 12:20:58 -0500

    removed gh-pages

--- js/app.js
+++ js/app.js
@@ -3,33 +3,33 @@ var app = angular.module('mainApp', ['ngRoute']);
 app.config(function($routeProvider) {
 
     $routeProvider.when('/portfolio/', {
-      templateUrl : 'https://raw.githubusercontent.com/blue2525989/portfolio/master/pages/home.html',
+      templateUrl : 'pages/home.html',
       controller : 'home',
       controllerAs: 'home'
     }).when('/projects', {
-      templateUrl : 'https://raw.githubusercontent.com/blue2525989/portfolio/master/pages/projects.html',
+      templateUrl : 'pages/projects.html',
       controller : 'projects',
       controllerAs: 'projects'
     }).when('/about', {
-    	templateUrl : 'https://raw.githubusercontent.com/blue2525989/portfolio/master/pages/about-me.html',
+    	templateUrl : 'pages/about-me.html',
     	controller : 'about',
     	controllerAs : 'about'
     }).when('/education', {
-    	templateUrl : 'https://raw.githubusercontent.com/blue2525989/portfolio/master/pages/education.html'
+    	templateUrl : 'pages/education.html'
     }).when('/project/blue-spa-webapp', {
-    	templateUrl : 'https://raw.githubusercontent.com/blue2525989/portfolio/master/pages/projects/blue-spa.html'
+    	templateUrl : 'pages/projects/blue-spa.html'
     }).when('/project/jb-webapp', {
-    	templateUrl : 'https://raw.githubusercontent.com/blue2525989/portfolio/master/pages/projects/jb-webapp.html'
+    	templateUrl : 'pages/projects/jb-webapp.html'
     }).when('/project/ttb-webapp', {
-    	templateUrl : 'https://raw.githubusercontent.com/blue2525989/portfolio/master/pages/projects/ttb-webapp.html'
+    	templateUrl : 'pages/projects/ttb-webapp.html'
     }).when('/project/ww-webapp', {
-    	templateUrl : 'https://raw.githubusercontent.com/blue2525989/portfolio/master/pages/projects/ww-webapp.html'
+    	templateUrl : 'pages/projects/ww-webapp.html'
     }).when('/project/cipher-app', {
-    	templateUrl : 'https://raw.githubusercontent.com/blue2525989/portfolio/master/pages/projects/text-file-cipher-app.html'
+    	templateUrl : 'pages/projects/text-file-cipher-app.html'
     }).when('/project/mysql-manager-app', {
-    	templateUrl : 'https://raw.githubusercontent.com/blue2525989/portfolio/master/pages/projects/mysql-db-manager-app.html'
+    	templateUrl : 'pages/projects/mysql-db-manager-app.html'
     }).when('/project/card-games-app', {
-    	templateUrl : 'https://raw.githubusercontent.com/blue2525989/portfolio/master/pages/projects/blue-spa.htmlpoker-blackjack-app.html'
+    	templateUrl : 'pages/projects/poker-blackjack-app.html'
     })
     .otherwise('/portfolio/');
 
